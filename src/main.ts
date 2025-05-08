84414fb5484c8cc3cc2158266e050d509114bf10
evenwan <evenwan0010@163.com>
2025-05-08 16:30:37 +0800

    build: import tdesign-vue-next style

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import "reset.css";
+import "tdesign-vue-next/es/style/index.css";
 import "./styles/global.scss";
 
 const app = createApp(App);
